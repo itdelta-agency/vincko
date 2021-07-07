444a72767a49adbffec04d10a7a963e768753665
root <root@578653-ck23400.tmweb.ru>
2021-07-07 17:55:21 +0300

    Updated new template files, created packages page

---
+++ packages/index.php
@@ -0,0 +1,24 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+?>
+Готовые решения
+<?$APPLICATION->IncludeComponent(
+    "it-delta:iblock.content",
+    "",
+    Array(
+        "ACTIVE_DATE" => "N",
+        "ADD_CACHE_STRING" => "",
+        "CACHE_TIME" => "0",
+        "CACHE_TYPE" => "A",
+        "FILTER_NAME" => "arrFilter1",
+        "IBLOCK_ID" => "34",
+        "IBLOCK_TYPE" => "references",
+        "PAGE_ELEMENT_COUNT" => "10",
+        "RAND_ELEMENTS" => "N",
+        "SORT_BY1" => "ACTIVE_FROM",
+        "SORT_BY2" => "SORT",
+        "SORT_ORDER1" => "DESC",
+        "SORT_ORDER2" => "ASC"
+    )
+);?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
