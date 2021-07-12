93ceb4402cc25fbce721d9a43f95866d386a1a90
root <root@578653-ck23400.tmweb.ru>
2021-07-12 16:59:48 +0300

    add bitrix templates

---
+++ local/templates/v_new_template/components/bitrix/menu/for_property/template.php
@@ -0,0 +1,23 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+
+<? if (!empty($arResult)): ?>
+<div class="header__submenu-item header__submenu-item--down">
+    <h3>
+        Для охраны коммерческой недвижимости
+    </h3>
+    <ul>
+        <?
+        foreach ($arResult as $arItem):
+            if ($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1)
+                continue;
+            ?>
+            <? if ($arItem["SELECTED"]): ?>
+            <li><a class="active" href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
+        <? else: ?>
+            <li><a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
+        <? endif ?>
+
+        <? endforeach ?>
+    </ul>
+</div>
+<? endif ?>
