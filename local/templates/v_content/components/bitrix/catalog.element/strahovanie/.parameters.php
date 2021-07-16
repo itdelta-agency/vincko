02cfa04165c8802ac0f5a1ed60cc9bf8e991bc78
Людмила Карпова <lyudmila.karpova.s@gmail.com>
2021-07-16 17:59:27 +0300

    [DEV3] Страница страхования

---
+++ local/templates/v_content/components/bitrix/catalog.element/strahovanie/.parameters.php
@@ -0,0 +1,13 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentParameters = array(
+	"PARAMETERS" => array(
+		"TITLE_POLICY" => array(
+			"PARENT" => "BASE",
+			"NAME" => GetMessage("P_INSURANCE_TITLE_POLICY"),
+			"TYPE" => "STRING",
+			"DEFAULT" => 'Доступные страховые полисы',
+		)
+	),
+);
---
+++ local/templates/v_content/components/bitrix/catalog.element/strahovanie/result_modifier.php
@@ -0,0 +1,15 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+use Vincko\Policy;
+
+// получим все варианты выплаты по основным пунктам страховки
+$arAllPaymentOptions = Policy::getPaymentOptions();
+
+// переберем все торговые предложения и сформируем необходимый массив
+if (!empty($arResult["OFFERS"])) {
+	foreach ($arResult["OFFERS"] as $arOffer) {
+		$arResult["POLICIES"][] = Policy::formatPolicy($arOffer, $arAllPaymentOptions);
+	}
+}
+
+?>
