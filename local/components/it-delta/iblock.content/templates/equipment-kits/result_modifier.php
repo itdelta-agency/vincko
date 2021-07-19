cbfdc0103aa113b5da0326e15269c4b522fed51e
root <root@578653-ck23400.tmweb.ru>
2021-07-19 12:36:11 +0300

    result equipment-kits

--- local/components/it-delta/iblock.content/templates/equipment-kits/result_modifier.php
+++ local/components/it-delta/iblock.content/templates/equipment-kits/result_modifier.php
@@ -1,55 +1,129 @@
 <?php
-$dbResSect = CIBlockSection::GetList(
-    Array("SORT"=>"ASC"),
-    Array("ACTIVE"=>"Y","IBLOCK_ID"=>$arParams['IBLOCK_ID'])
+//Подготовка классов решений
+$arrClassFetch = CIBlockElement::GetList(
+    array("SORT" => "ASC"),
+    array("IBLOCK_ID" => "36"),
+    false,
+    false,
+    array("ID", "NAME", "PREVIEW_PICTURE")
 );
+while ($class = $arrClassFetch->GetNext()) {
+    $arrClass[$class['ID']] = $class;
+    $arrClass[$class['ID']]['PREVIEW_PICTURE'] = CFile::GetPath($class["PREVIEW_PICTURE"]);
+}
+//Подготовка характеристик разделов
+$arrCharacteristicsFetch = CIBlockElement::GetList(
+    array("SORT" => "ASC"),
+    array("IBLOCK_ID" => "39"),
+    false,
+    false,
+    array("ID", "NAME", "PREVIEW_PICTURE")
+);
+while ($class = $arrCharacteristicsFetch->GetNext()) {
+    $arrCharacteristics[$class['ID']] = $class;
+    $arrCharacteristics[$class['ID']]['PREVIEW_PICTURE'] = CFile::GetPath($class["PREVIEW_PICTURE"]);
+}
+
 //получаем разделы и собираем в массив
-while($sectRes = $dbResSect->GetNext())
-{
+$dbResSect = CIBlockSection::GetList(
+    array("SORT" => "ASC"),
+    array("ACTIVE" => "Y", "IBLOCK_ID" => $arParams['IBLOCK_ID']),
+    false,
+    array('UF_CHARACTERISTICS_REF'),
+    false
+);
+while ($sectRes = $dbResSect->GetNext()) {
+    //Заполняем разделы характеристиками с инфоблока
+    foreach ($sectRes['UF_CHARACTERISTICS_REF'] as $uf):
+        if (!empty($arrCharacteristics[$uf]['PREVIEW_PICTURE'])):
+            $sectRes['CHARACTERISTICS'][] = $arrCharacteristics[$uf];
+        else:
+            $arrWithoutImage[] = $arrCharacteristics[$uf];
+        endif;
+    endforeach;
+    $sectRes['CHARACTERISTICS'] = array_merge($sectRes['CHARACTERISTICS'], $arrWithoutImage);
     //получаем путь картинки раздела
     $sectRes['PICTURE_SRC'] = CFile::GetPath($sectRes["PICTURE"]);
     $arSections[$sectRes['ID']] = $sectRes;
 }
-//прикрепляем элементы к разделам
-foreach($arResult["ITEMS"] as $key => $arItem) {
+
+
+foreach ($arResult["ITEMS"] as $key => $arItem) {
     $arSections[$arItem['IBLOCK_SECTION_ID']]['ITEMS'][] = $arItem;
+
+    $arSections[$arItem['IBLOCK_SECTION_ID']]['CLASS_INFO'] = $arrClass[$arItem['PROPERTIES']['CO_CLASS_REF']['VALUE']];
 }
+
 
 //собираем id комплектов из всех готовых решений
 $equipmentKitsIds = array();
 foreach ($arSections as $key => $arSection) {
-    foreach ($arSection['ITEMS'] as $key => $arItem) {
+    foreach ($arSection['ITEMS'] as $arItem) {
         $item = $arItem['PROPERTIES']['P_COMPLECT']['VALUE'];
-        if(is_array($item))
+        if (is_array($item))
             $equipmentKitsIds = array_merge($equipmentKitsIds, $item);
+        foreach ($arItem['PROPERTIES']['P_COMPLECT']['VALUE'] as $complect):
+            $arConnect[$complect] = $arrClass[$arItem['PROPERTIES']['CO_CLASS_REF']['VALUE']];
+        endforeach;
     }
 }
 
 $dbResEquipmentKits = CIBlockElement::GetList(
-    Array("SORT"=>"ASC"),
-    Array("ACTIVE"=>"Y","IBLOCK_ID"=>$arParams['EQUIPMENT-KITS_IBLOCK_ID'], "ID" =>$equipmentKitsIds)
+    array("SORT" => "ASC"),
+    array("ACTIVE" => "Y", "IBLOCK_ID" => $arParams['EQUIPMENT-KITS_IBLOCK_ID'], "ID" => $equipmentKitsIds),
+    false,
+    false,
+    array("ID", "NAME", "PROPERTY_CO_CHARACTERISTICS_REF", "CODE", "PROPERTY_CO_PRESENTS_REF", 'CATALOG_GROUP_1')
 );
 
-while($equipmentKitsRes = $dbResEquipmentKits->GetNext())
-{
+$arCharacteristic = array();
+while ($equipmentKitsRes = $dbResEquipmentKits->GetNext()) {
     //получаем путь картинки
-    $equipmentKitsRes['PICTURE_SRC'] = CFile::GetPath($equipmentKitsRes["PREVIEW_PICTURE"]);
+    $equipmentKitsRes['DETAIL_URL'] = $equipmentKitsRes['CODE'];
+
+    $equipmentKitsRes['PROPERTY_CO_CLASS_REF_VALUE'] != null ? $arrComplect[] = $equipmentKitsRes['PROPERTY_CO_CLASS_REF_VALUE'] : '';
+    $equipmentKitsRes['PROPERTY_CO_CHARACTERISTICS_REF_VALUE'] != null ? $arCharacteristic = array_merge($arCharacteristic, $equipmentKitsRes['PROPERTY_CO_CHARACTERISTICS_REF_VALUE']) : '';
+
     $arEquipmentKits[$equipmentKitsRes['ID']] = $equipmentKitsRes;
 }
+
+$characteristicsFetch = CIBlockElement::GetList(
+    array("SORT" => "ASC"),
+    array("IBLOCK_ID" => "37", "ID" => $arCharacteristic),
+    false,
+    false,
+    array("ID", "NAME", "PREVIEW_PICTURE")
+);
+while ($detail = $characteristicsFetch->GetNext()) {
+    $characteristicDetail[$detail['ID']] = $detail;
+    $characteristicDetail[$detail['ID']]["PREVIEW_PICTURE"] = CFile::GetPath($detail["PREVIEW_PICTURE"]);
+}
+
+
+foreach ($arEquipmentKits as $eq):
+    foreach ($eq['PROPERTY_CO_CHARACTERISTICS_REF_VALUE'] as $characteristicID):
+        $arEquipmentKits[$eq['ID']]['COMPLECT_CHARACTERISTICS'][$characteristicID] = $characteristicDetail[$characteristicID];
+    endforeach;
+    $arEquipmentKits[$eq['ID']]['CLASS_INFO'] = $arConnect[$eq['ID']];
+endforeach;
+
+
 //прикрепляем комплекты к разделам
 foreach ($arSections as $key => $arSection) {
-    foreach ($arSection['ITEMS'] as $key => $arItem) {
+    foreach ($arSection['ITEMS'] as $arItem) {
         $item = $arItem['PROPERTIES']['P_COMPLECT']['VALUE'];
-        if(is_array($item))
-        {
-            foreach ($item as $key => $value) {
-                if(key_exists($value,$arEquipmentKits))
+        if (is_array($item)) {
+            foreach ($item as $value) {
+                if (key_exists($value, $arEquipmentKits))
                     $arSections[$arSection['ID']]['EQUIPMENT-KITS'][] = $arEquipmentKits[$value];
+
             }
         }
     }
 }
-
+//echo "<pre>";
+//print_r($arSections);
+//echo "</pre>";
 $arResult["SECTIONS"] = $arSections;
 
 
