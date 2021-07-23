d7278edad6d6ad55607771362433c310bb4353ba
worldandlife <v0v41999@gmail.com>
2021-07-23 18:47:50 +0300

    Minor changes

--- ajax/addorder.php
+++ ajax/addorder.php
@@ -1,5 +1,9 @@
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
-print_r($_POST);
+
+$request = \Bitrix\Main\Application::getInstance()->getContext()->getRequest();
+//print_r(json_encode($_POST));
+//print_r($request);
+
 //$complect_id = intval($_POST['order-id']['complect_id']);
 //$guard_id = intval($_POST['order-id']['guard_id']);
 //$insurance_id = intval($_POST['order-id']['insurance_id']);
@@ -12,4 +16,9 @@ print_r($_POST);
 //        Add2BasketByProductID($insurance_id, 1, false);
 //    }
 //}
+
+$res = ["result" => "OK"];
+$response = new \Bitrix\Main\Engine\Response\Json($res);
+$response->send();
+
 ?>
