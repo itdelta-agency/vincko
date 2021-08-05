25b0f7623b79038027fc15961594f19eae9fd039
IgorKamalin <prog4@it-delta.ru>
2021-08-05 17:41:05 +0300

    t751 ïðàâêè õåäåð è ôóòåð

--- include/vincko_citymodal.php
+++ include/vincko_citymodal.php
@@ -13,15 +13,42 @@ $cityList = \Bitrix\Iblock\ElementTable::getList(array(
 	))->fetchAll();
 ?>
 <div id="header__top-info" class="header__top-info--popup modal">
-    <div class="modal__content">
+<!--    <div class="modal__content">-->
         <div data-close-button class="close-button"></div>
-        <div class="modal__content-body">
-            <h4>Выберите город</h4>
-            <ul class="menu__list">
-                <?foreach ($cityList as $aCity):?>
-                <li class="<?=$GLOBALS["GEOCITY"]["ID"] == $aCity["ID"] ? 'active' : ''?>"><a data-id="<?=$aCity["ID"]?>" class="js-get-city" href="#"><?=$aCity["NAME"]?></a></li>
-                <?endforeach;?>
-            </ul>
+        <div class="modal__content-body searchForm__modal">
+<!--            <h4>Выберите город</h4>-->
+<!--            <ul class="menu__list">-->
+<!--                <li class="--><?//=$GLOBALS["GEOCITY"]["ID"] == $aCity["ID"] ? 'active' : ''?><!--"><a data-id="--><?//=$aCity["ID"]?><!--" class="js-get-city" href="#">--><?//=$aCity["NAME"]?><!--</a></li>-->
+<!--            </ul>-->
+            <div data-close-button class="close-button"></div>
+            <div class="searchForm__modal_input">
+                <input type="text" id="filterCity" placeholder="Поиск по названию">
+                <button>
+                    <img src="../img/header/search-icon.svg" alt="img">
+                </button>
+            </div>
+            <div class="searchForm__modal_wrapper">
+                <div class="searchForm__modal_topChek active">
+                    <div class="searchForm__modal_item topChekItem">
+                        <input type="checkbox" class="checkbox">
+                        <span class="itemText">Санкт-Петербург</span>
+                    </div>
+                </div>
+                <div class="searchForm__modal_centerChek">
+                    <div class="searchForm__modal_item">
+                        <input type="checkbox" class="checkbox">
+                        <span class="itemText">Санкт-Петербург</span>
+                    </div>
+                </div>
+                <div class="searchForm__modal_bottomChek">
+                    <?foreach ($cityList as $aCity):?>
+                        <div class="searchForm__modal_item bottomChekItem">
+                            <input type="checkbox" class="checkbox" value="<?=$aCity["ID"]?>">
+                            <span class="itemText"><?=$aCity["NAME"]?></span>
+                        </div>
+                    <?endforeach;?>
+                </div>
+            </div>
         </div>
-    </div>
+<!--    </div>-->
 </div>
